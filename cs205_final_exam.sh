244fe2cacefdb24461db4243a0ccc5982a717559
kmd566 <kmd566@nau.edu>
2021-11-08 02:00:45 -0700

    wrote a sorting and analysis program and put a screenshot of the results within a folder titled screenshots.

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,3 +1,4 @@
+
 # TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
 # ======= SUMMARY OF POKEMON.DAT ======
 #    Total Pokemon: [VALUE]
@@ -9,3 +10,15 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+file_path="/home/kmd566/CS205/cs205_final/pokemon.dat"
+
+echo "======= SUMMARY OF POKEMON.DAT ======"
+
+awk 'BEGIN{total=0} //{total++} END{print "Total Pokemon: ",total}' $file_path
+
+awk '{sum+=$4} END { print "Avg. HP: = ",sum/NR}' $file_path
+
+awk '{sum+=$5} END { print "Avg. Attack: = ",sum/NR}' $file_path
+
+echo "======= END SUMMARY ======="
